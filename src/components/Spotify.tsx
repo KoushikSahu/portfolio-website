51415c345c69799af32633418540cf793780b5e9
Koushik Sahu <koushiksahu68@gmail.com>
2021-04-23 16:29:10 +0530

    added iframe title

--- src/components/Spotify.tsx
+++ src/components/Spotify.tsx
@@ -5,7 +5,7 @@ function Spotify(): JSX.Element {
 	return (
 		<div>
 			<h1 className="heading-text">Currently Listening To...</h1>
-			<iframe src="https://open.spotify.com/embed/playlist/6ASOWj2GN7fjgeweHKadn9" width={ 300 } height={ 380 } frameBorder={ 0 } allowTransparency={ true } allow="encrypted-media" />
+			<iframe src="https://open.spotify.com/embed/playlist/6ASOWj2GN7fjgeweHKadn9" title="spotify-embed" width={ 300 } height={ 380 } frameBorder={ 0 } allowTransparency={ true } allow="encrypted-media" />
 		</div>
 	)
 }
